471ce203efe27e589f7dda003d4a16ffb3ab6656
rudarmonga <rudar.arora12@gmail.cam>
2025-07-26 00:50:27 +0530

    Todo Model and CRUD

---
+++ backend/controllers/todoController.js
@@ -0,0 +1,47 @@
+const Todo = require('../models/Todo');
+
+exports.getTodos = async (req, res) => {
+    try {
+        const todos = await Todo.find();
+        res.json(todos);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
+
+exports.createTodo = async (req, res) => {
+    try {
+        const { title } = req.body;
+        const newTodo = new Todo({ title});
+        await newTodo.save();
+        res.status(201).json(newTodo);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+}
+
+exports.updateTodo = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const updatedTodo = await Todo.findByIdAndUpdate(id, req.body, { new: true });
+        if (!updatedTodo) {
+            return res.status(404).json({ message: 'Todo not found' });
+        }
+        res.json(updatedTodo);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+}
+
+exports.deleteTodo = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const deletedTodo = await Todo.findByIdAndDelete(id);
+        if (!deletedTodo) {
+            return res.status(404).json({ message: 'Todo not found' });
+        }
+        res.json({ message: 'Todo deleted successfully' });
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+}
